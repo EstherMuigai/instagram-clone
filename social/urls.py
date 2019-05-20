5988c8d59359e7fc502a2dde238bf0cf2665894a
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-05-20 15:46:44 +0300

    Install the django-registrationmodule,configure a root path and set up a base template.

--- social/urls.py
+++ social/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'',include('instagram.urls'))
 ]
 
